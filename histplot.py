03a7c7a96fb1b893be21d3388a7b495591a91af3
bercev <ercevik.berat@gmail.com>
2025-08-22 15:01:01 -0700

    Added histplot.py and added a file to .gitignore

--- histplot.py
+++ histplot.py
@@ -2,15 +2,27 @@ import ijson
 import matplotlib.pyplot as plt
 
 thread_lengths = []
-
+longest_threads = []
+get_tweets = True
 with open("./grok_data/data.json", "r") as f:
     # stream through "threads.item.tweets" arrays
     for thread in ijson.items(f, "item.threads.item"):
+        threadId = thread.get("threadId", "")
+        
         tweets = thread.get("tweets", [])
-        thread_lengths.append(len(tweets))
+        length_of_current_tweeet = len(tweets)
+        if (length_of_current_tweeet >= 40) and get_tweets:   # get 3 tweets > 40
+            longest_threads.append(threadId)
+            if (len(longest_threads) > 3):
+                get_tweets = False
+        thread_lengths.append(length_of_current_tweeet)
 
 # Plot histogram
-print(len(thread))
+print("Number conversations: " + str(len(thread)))
+print("Longest threads:")
+for i,thd in enumerate(longest_threads):
+    print(thd)
+    print("\n\n\n\n")
 plt.figure(figsize=(8, 6))
 plt.hist(thread_lengths, bins=range(1, max(thread_lengths) + 2), edgecolor='black', alpha=0.7)
 plt.title("Distribution of Thread Lengths")
